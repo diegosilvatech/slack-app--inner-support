d7cde5a9a823df4119a6db0dabf08c175c996cc7
Diego Silva <social.diegosilvatech@gmail.com>
2022-08-01 16:34:46 -0300

    remove unused logs

--- src/features/commands.js
+++ src/features/commands.js
@@ -6,14 +6,7 @@ function initCommands(app) {
     try {
       if (command.text === 'issues --new') {
         await ack();
-        // const requesterData = {
-        //   user_id: command.user_id,
-        //   user_name: command.user_name,
-        //   channel_id: command.channel_id,
-        //   channel_name: command.channel_name
-        // };
 
-        // console.log('RESULT', requesterData);
         await modalCreateNewIssue(client, body);
       }
 
